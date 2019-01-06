d5af61e99981c8c38f34c0ad79cd2b5007ab3b33
Jamie Land (Desktop) <jamie.land@lunastone.space>
2019-01-06 12:18:59 +0000

    update

--- js/main.js
+++ js/main.js
@@ -7,3 +7,33 @@ function init() {
 
 	$(".scroller a").preventDefault();
 }
+
+function hover_d(element) {
+	var i = 2;
+	while (true) {
+		if (i > 3) {
+			i = 1;
+		}
+		element.setAttribute('src', '../images/downarrow-' + i + '.png');
+		i++;
+	}
+}
+
+function unhover_d(element) {
+	element.setAttribute('src', '../images/downarrow-1.png');
+}
+
+function hover_u(element) {
+	var i = 2;
+	while (true) {
+		if (i > 3) {
+			i = 1;
+		}
+		element.setAttribute('src', '../images/uparrow-' + i + '.png');
+		i++;
+	}
+}
+
+function unhover_u(element) {
+	element.setAttribute('src', '../images/uparrow-1.png');
+}
